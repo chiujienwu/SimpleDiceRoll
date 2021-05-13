b477d230bcdeecfca6f7f2e8fd3b3d11369e2e80
chiujienwu <jchiu1@my.wctc.edu>
2021-05-13 12:48:37 -0500

    completed binding by code behind

--- SimpleDiceRoll/MainPage.xaml.cs
+++ SimpleDiceRoll/MainPage.xaml.cs
@@ -16,37 +16,38 @@ namespace SimpleDiceRoll
         {
             InitializeComponent();
 
-            // binding by code or by xaml
+            //binding by code or by xaml
+
+            //source object (1) and could be loaded from  serivce
 
             Die die = new Die
             {
                 Sides = 6,
                 Rolls = 3
-            }
-                ;  // source object (1) and could be loaded from  serivce
+            };
 
-/*            // binding by code in the code behind is demonstrated below
+            // binding by code in the code behind is demonstrated below
 
-            Entry dieSidesEntry = new Entry();  // target object (3), the entry
-            
-            Binding sidesBinding = new Binding();
-            sidesBinding.Source = die;
-            sidesBinding.Path = "Sides";  // source object property (2)
-            SidesEntry.SetBinding(Entry.TextProperty, sidesBinding);  // find UI View to Codebehind Source Object
+            // Entry dieSidesEntry = new Entry();  // target object (3), the entry and can be in xaml
+
+            Binding dieSidesBinding = new Binding();
+            dieSidesBinding.Source = die;
+            dieSidesBinding.Path = "Sides";  // source object property (2)
+            SidesEntry.SetBinding(Entry.TextProperty, dieSidesBinding);  // find UI View to Codebehind Source Object
 
 
-            Binding rollsBinding = new Binding();
-            rollsBinding.Source = die;
-            rollsBinding.Path = "Rolls";
-            RollsEntry.SetBinding(Entry.TextProperty, rollsBinding);  // find UI View to Codebehind Source Object
+            Binding dieRollsBinding = new Binding();
+            dieRollsBinding.Source = die;
+            dieRollsBinding.Path = "Rolls";
+            RollsEntry.SetBinding(Entry.TextProperty, dieRollsBinding);  // find UI View to Codebehind Source Object
 
             // TODO:  NEED METHOD FOR CREATING ROLL RESULTS AND BIND TO VIEW THE RESULTS OF ROLLS
 
-            DisplayAlert("Binding Set", "Die sides = " + die.Sides + ", Num of rolls = " + die.Rolls, "OK");*/
+            DisplayAlert("Binding Set", "Die sides = " + die.Sides + ", Num of rolls = " + die.Rolls, "OK");
 
         }
 
-        private void RollDiceButton (int sides, int rolls)
+        public void RollDiceButton(object sender, EventArgs e)
         {
             
             Random rnd = new Random();
