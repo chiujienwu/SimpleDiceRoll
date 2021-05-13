0a3cea0824597f000707dcbb1dd0e07fb101a999
chiujienwu <jchiu1@my.wctc.edu>
2021-05-13 12:53:18 -0500

    completed example of binding through code behind

--- SimpleDiceRoll/MainPage.xaml.cs
+++ SimpleDiceRoll/MainPage.xaml.cs
@@ -16,43 +16,33 @@ namespace SimpleDiceRoll
         {
             InitializeComponent();
 
-            //binding by code or by xaml
+            //binding by code
 
             //source object (1) and could be loaded from  serivce
-
             Die die = new Die
             {
                 Sides = 6,
                 Rolls = 3
             };
 
-            // binding by code in the code behind is demonstrated below
-
-            // Entry dieSidesEntry = new Entry();  // target object (3), the entry and can be in xaml
-
             Binding dieSidesBinding = new Binding();
             dieSidesBinding.Source = die;
-            dieSidesBinding.Path = "Sides";  // source object property (2)
-            SidesEntry.SetBinding(Entry.TextProperty, dieSidesBinding);  // find UI View to Codebehind Source Object
+            dieSidesBinding.Path = "Sides";  //source object property (2)
+            SidesEntry.SetBinding(Entry.TextProperty, dieSidesBinding);  //target is specified by name SidesEntry (3) and the property is the Entry's TextProperty (4)
 
-
+            //second binding to the same source object but different property, target and target property
             Binding dieRollsBinding = new Binding();
             dieRollsBinding.Source = die;
             dieRollsBinding.Path = "Rolls";
-            RollsEntry.SetBinding(Entry.TextProperty, dieRollsBinding);  // find UI View to Codebehind Source Object
+            RollsEntry.SetBinding(Entry.TextProperty, dieRollsBinding);
 
             // TODO:  NEED METHOD FOR CREATING ROLL RESULTS AND BIND TO VIEW THE RESULTS OF ROLLS
-
             DisplayAlert("Binding Set", "Die sides = " + die.Sides + ", Num of rolls = " + die.Rolls, "OK");
-
         }
 
         public void RollDiceButton(object sender, EventArgs e)
-        {
-            
+        {            
             Random rnd = new Random();
-
         }
-
     }
 }
