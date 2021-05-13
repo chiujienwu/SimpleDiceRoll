97b483d278cf816b4d38c349d9a282a0171a2164
chiujienwu <jchiu1@my.wctc.edu>
2021-05-13 14:00:52 -0500

    example of view to view binding of slider and label of slider value on same page

--- SimpleDiceRoll/App.xaml.cs
+++ SimpleDiceRoll/App.xaml.cs
@@ -10,7 +10,7 @@ namespace SimpleDiceRoll
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new ViewToViewBindingPage();
         }
 
         protected override void OnStart()
